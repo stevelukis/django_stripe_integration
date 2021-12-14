7c279f2cff77c18bd58675f84fbcf4b0c815b2d9
Steve Lukis <stevelukis88@gmail.com>
2021-12-14 15:06:37 +0800

    Add image field

--- product/models.py
+++ product/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10,
                                 decimal_places=2)
+    image = models.ImageField(upload_to='products',
+                              blank=True,
+                              null=True)
