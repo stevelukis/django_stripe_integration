f6f1b9b9501ff35f9289358f741d89de3fa1eade
Steve Lukis <stevelukis88@gmail.com>
2021-12-15 10:54:28 +0800

    Add sale app and model

---
+++ sales/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+from product.models import Product
+
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    checkout_session = models.CharField(max_length=200, unique=True)
+    paid = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
