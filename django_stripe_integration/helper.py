1439d26852dca0b1a8b88803685a3e4e8414ea69
Steve Lukis <stevelukis88@gmail.com>
2021-12-15 10:05:06 +0800

    Add Stripe fee of 2.7%

---
+++ django_stripe_integration/helper.py
@@ -0,0 +1,2 @@
+def get_service_fee(price: int):
+    return int(0.027 * price) + 30
--- django_stripe_integration/views.py
+++ django_stripe_integration/views.py
@@ -4,7 +4,7 @@ from rest_framework import generics
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
-from django_stripe_integration import settings
+from django_stripe_integration import settings, helper
 from product.models import Product
 
 
@@ -20,6 +20,7 @@ class StripeCheckoutSession(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         product_id = kwargs['product']
         product = Product.objects.get(id=product_id)
+        price = int(product.price * 100)
         domain_url = 'http://127.0.0.1:8000/'
         stripe.api_key = settings.STRIPE_SECRET_KEY
         try:
@@ -33,7 +34,7 @@ class StripeCheckoutSession(generics.GenericAPIView):
                         'name': product.name,
                         'quantity': 1,
                         'currency': 'usd',
-                        'amount': int(product.price) * 100,
+                        'amount': price + helper.get_service_fee(price),
                     }
                 ]
             )
